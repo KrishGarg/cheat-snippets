3bb49572ec5cb54637858ba3899cf73364b779bb
Krish Garg <krishgarg6306@gmail.com>
2021-10-12 23:05:51 +0530

    ➕ Added react-bootstrap and firebase

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize app
+const app = initializeApp(firebaseConfig);
+
+// Auth provider and instance
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+// Firestore db instance
+const db = getFirestore(app);
+
+export { provider, auth, db };
